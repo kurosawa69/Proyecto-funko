5a6a20f9ff22bd36ec74f6bb57a0e67b089111d5
Adrianaelis29@gmail.com <adrianaelis29@gmail.com>
2023-11-30 23:08:38 -0300

    modifica route del shop

--- src/routes/shop.routes.js
+++ src/routes/shop.routes.js
@@ -1,12 +1,11 @@
-const express = require('express')
-const router = express.Router()
-const shopController = require('../controllers/shop.controller')
+const express = require("express");
+const router = express.Router();
+const shopController = require("../controllers/shop.controller");
 
+router.get("/", shopController.shop);
+router.get("/item/:id", (req, res) => res.send("route for item id view"));
+router.post("/item/:id/add", (req, res) => res.send("route for agregar view"));
+router.get("/cart", (req, res) => res.send("route for carrito view"));
+router.post("/cart", (req, res) => res.send("route for add id to cart view "));
 
-router.get('/shop', shopController.shop)
-router.get('/shop/item/:id', (req, res) => res.send('route for item id view'))
-router.post('/shop/item/:id/add', (req, res) => res.send('route for agregar view'))
-router.get('/shop/cart', (req, res) => res.send('route for carrito view'))
-
-
-module.exports = router
+module.exports = router;
