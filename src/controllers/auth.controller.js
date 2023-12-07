6ccb4526050723b088b1631c64966f8383dc829a
Maxi Arrieta <diegomarrieta69@gmail.com>
2023-12-06 23:23:48 -0300

    movi register login y carrito a views y modifique rutas para que se vean

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 module.exports = {
     login: (req, res) => {
-        res.render('login')
+        res.render(path.resolve(__dirname, "../views/auth/login.ejs"))
         
     },
     register: (req, res) => {
--- src/routes/shop.routes.js
+++ src/routes/shop.routes.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const shopController = require("../controllers/shop.controller");
 
-router.get("/", shopController.shop);
+router.get("/shop", shopController.shop);
 router.get("/item/:id", (req, res) => res.send("route for item id view"));
 router.post("/item/:id/add", (req, res) => res.send("route for agregar view"));
-router.get("/cart", (req, res) => res.send("route for carrito view"));
+router.get("/cart", shopController.cart);
 router.post("/cart", (req, res) => res.send("route for add id to cart view "));
 
 module.exports = router;
