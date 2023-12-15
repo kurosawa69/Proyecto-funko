2ebb26632e0e81456565d12a9176ff2d644294f0
solcedro82 <solcedro82@gmail.com>
2023-12-15 08:47:34 -0300

    configuración rutas sol

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
 
-router.get('/admin', (req, res) => res.send('route for admin view'))
-router.get('/admin/create', (req, res) => res.send('route for admin create view'))
-router.post('/admin/create', (req, res) => res.send('route for admin create post view'))
-router.get('/admin/edit/:id', (req, res) => res.send('route for admin edit view'))
-router.put('/admin/edit/:id', (req, res) => res.send('route for admin edit post view'))
-router.delete('/admin/delete/:id', (req, res) => res.send('route for admin delete view'))
+router.get('/admin', (req, res) => res.send('route for admin view'));
+router.get('/admin/create', (req, res) => res.send('route for admin create view'));
+router.post('/admin/create', (req, res) => res.send('route for admin create post view'));
+router.get('/admin/edit/:id', (req, res) => res.send('route for admin edit view'));
+router.put('/admin/edit/:id', (req, res) => res.send('esta es la vista para hacer efectiva la modificación de un item'));
+router.delete('/admin/delete/:id', (req, res) => res.send('esta es la ruta para borrar un item específico'));
 
-module.exports = router
+module.exports = router;
