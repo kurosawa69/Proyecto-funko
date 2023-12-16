a70b217dcca497b3f40cab9d1afef87ac96fa3e8
solcedro82 <solcedro82@gmail.com>
2023-12-16 18:26:13 -0300

    controladores admin y borro pages de vistas estáticas

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -1,11 +1,14 @@
+const path= require ('path');
 module.exports = {
 
-    admin: (req, res) => res.send('esta es la vista de admin'),
-    create: (req, res) => res.send('esta es la vista de crear un nuevo item'),
+    admin: (req, res) => res.render (path.resolve(__dirname, "../views/admin/adm.ejs"),{
+        tittle: "FunkoAdmin"
+    }),
+    create: (req, res) => res.render (path.resolve(__dirname, "../views/admin/creat.ejs")),
     agregar: (req, res) => res.send('esta es la ruta para agregar un nuevo item'),
-    edit: (req, res) => res.send('esta es la vista de editar un item'),
-    modificación: (req, res) => res.send('esta es la vista para hacer efectiva la modificación de un item'),
-    delete: (req, res) => res.send('esta es la ruta para borrar un item específico'),
+    edit: (req, res) => res.render (path.resolve(__dirname, "../views/admin/edit.ejs")),
+    itemModificación: (req, res) => res.send('esta es la vista para hacer efectiva la modificación de un item'),
+    deleteItem: (req, res) => res.send('esta es la ruta para borrar un item específico'),
 
 
 }
--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/admin.controller');
-const adminController = require('../controllers/admin.controller');
+
+const { admin, 
+        create, 
+        agregar, 
+        edit, 
+        itemModificación, 
+        deleteItem 
+    } = require ('../controllers/admin.controller');
 
 
-router.get('/admin', adminController.admin);
-router.get('/admin/create', adminController.create);
-router.post('/admin/create', adminController.agregar);
-router.get('/admin/edit/:id', adminController.edit);
-router.put('/admin/edit/:id', adminController.modificación);
-router.delete('/admin/delete/:id', adminController.delete);
+router.get('/admin', admin);
+router.get('/admin/create', create);
+router.post('/admin/create', agregar);
+router.get('/admin/edit/:id', edit);
+router.put('/admin/edit/:id', itemModificación);
+router.delete('/admin/delete/:id', deleteItem);
 
 module.exports = router;
