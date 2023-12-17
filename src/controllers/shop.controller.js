e6ab72fae6a8c6af3a893599444748bcb2492cf2
abdonsanchez <abdonsanchezuno@gmail.com>
2023-12-17 14:57:03 -0300

    vista item

--- src/controllers/shop.controller.js
+++ src/controllers/shop.controller.js
@@ -2,12 +2,33 @@ const fs = require("fs");
 const path = require("path");
 const { title } = require("process");
 
-module.exports = {
-  shop: (req, res) => {
-
-    const data = [
-      {
-      product_id: 1,
+const data = [
+  {
+  product_id: 1,
+  licence_name: "Pokemon",
+  category_name: "Figuras coleccionables",
+  product_name: "Pidgeotto",
+  product_description: "Figura coleccionable pokemon",
+  product_price: 1799.99,
+  dues: 10,
+  product_sku: "PKM001001",
+  img_front: "/img/pokemon/pidgeotto-1.webp",
+  img_back: "/img/pokemon/pidgeotto-box.webp",
+  },
+  {
+    product_id: 2,
+    licence_name: "Pokemon",
+    category_name: "Figuras coleccionables",
+    product_name: "FAlopero",
+    product_description: "Figura coleccionable pokemon",
+    product_price: 1799.99,
+    dues: 10,
+    product_sku: "PKM001001",
+    img_front: "/img/pokemon/pidgeotto-1.webp",
+    img_back: "/img/pokemon/pidgeotto-box.webp",
+    },
+    {
+      product_id: 3,
       licence_name: "Pokemon",
       category_name: "Figuras coleccionables",
       product_name: "Pidgeotto",
@@ -19,10 +40,10 @@ module.exports = {
       img_back: "/img/pokemon/pidgeotto-box.webp",
       },
       {
-        product_id: 2,
+        product_id: 4,
         licence_name: "Pokemon",
         category_name: "Figuras coleccionables",
-        product_name: "FAlopero",
+        product_name: "Pidgeotto",
         product_description: "Figura coleccionable pokemon",
         product_price: 1799.99,
         dues: 10,
@@ -31,7 +52,7 @@ module.exports = {
         img_back: "/img/pokemon/pidgeotto-box.webp",
         },
         {
-          product_id: 3,
+          product_id: 5,
           licence_name: "Pokemon",
           category_name: "Figuras coleccionables",
           product_name: "Pidgeotto",
@@ -43,7 +64,7 @@ module.exports = {
           img_back: "/img/pokemon/pidgeotto-box.webp",
           },
           {
-            product_id: 4,
+            product_id: 6,
             licence_name: "Pokemon",
             category_name: "Figuras coleccionables",
             product_name: "Pidgeotto",
@@ -55,7 +76,7 @@ module.exports = {
             img_back: "/img/pokemon/pidgeotto-box.webp",
             },
             {
-              product_id: 5,
+              product_id: 7,
               licence_name: "Pokemon",
               category_name: "Figuras coleccionables",
               product_name: "Pidgeotto",
@@ -67,7 +88,7 @@ module.exports = {
               img_back: "/img/pokemon/pidgeotto-box.webp",
               },
               {
-                product_id: 6,
+                product_id: 8,
                 licence_name: "Pokemon",
                 category_name: "Figuras coleccionables",
                 product_name: "Pidgeotto",
@@ -79,7 +100,7 @@ module.exports = {
                 img_back: "/img/pokemon/pidgeotto-box.webp",
                 },
                 {
-                  product_id: 7,
+                  product_id: 9,
                   licence_name: "Pokemon",
                   category_name: "Figuras coleccionables",
                   product_name: "Pidgeotto",
@@ -89,45 +110,35 @@ module.exports = {
                   product_sku: "PKM001001",
                   img_front: "/img/pokemon/pidgeotto-1.webp",
                   img_back: "/img/pokemon/pidgeotto-box.webp",
-                  },
-                  {
-                    product_id: 8,
-                    licence_name: "Pokemon",
-                    category_name: "Figuras coleccionables",
-                    product_name: "Pidgeotto",
-                    product_description: "Figura coleccionable pokemon",
-                    product_price: 1799.99,
-                    dues: 10,
-                    product_sku: "PKM001001",
-                    img_front: "/img/pokemon/pidgeotto-1.webp",
-                    img_back: "/img/pokemon/pidgeotto-box.webp",
-                    },
-                    {
-                      product_id: 9,
-                      licence_name: "Pokemon",
-                      category_name: "Figuras coleccionables",
-                      product_name: "Pidgeotto",
-                      product_description: "Figura coleccionable pokemon",
-                      product_price: 1799.99,
-                      dues: 10,
-                      product_sku: "PKM001001",
-                      img_front: "/img/pokemon/pidgeotto-1.webp",
-                      img_back: "/img/pokemon/pidgeotto-box.webp",
-                      }
-      
-      ]
+                  }
+  
+  ]
+
+module.exports = {
+  shop: (req, res) => {
+
+
     res.render(path.resolve(__dirname, "../views/shop/shop.ejs"), {
       title: "FunkoShop",
       data
     });
   },
   item: (req, res) => {
-    res.render(path.resolve(__dirname, "../views/item.ejs"),{
-    title: "Itemview"})
+    const itemId = req.params.id;
+    const item = data.find(item => item.product_id == itemId)
+
+    res.render(path.resolve(__dirname, "../views/shop/item.ejs"),{
+    title: "ItemView",
+    item
+  });
+  },
+  prueba: (req, res) => {
+    res.render(path.resolve(__dirname, "../views/shop/prueba.ejs"),{
+    title: "prueba"});
   },
   cart: (req, res) => {
     res.render(path.resolve(__dirname, "../views/cart.ejs"), {
       title: "FunkoCart"
-    });
-  }
+    })
+  },
 };
--- src/routes/shop.routes.js
+++ src/routes/shop.routes.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const shopController = require("../controllers/shop.controller");
 
 router.get("/shop", shopController.shop);
-router.get("/item/:id", shopController.item );
+router.get("/shop/item/:id", shopController.item );
+router.get("/shop/prueba", shopController.prueba );
 router.post("/item/:id/add", (req, res) => res.send("route for agregar view"));
 router.get("/cart", shopController.cart);
 router.post("/cart", (req, res) => res.send("route for add item to cart view "));
